59c78427c3b03837c2227d95eff8d8d482617076
juugeebee <44608893+juugeebee@users.noreply.github.com>
2019-10-22 15:15:45 +0200

    Construction de l’arbre de de Bruijn

--- debruijn/debruij.py
+++ debruijn/debruij.py
@@ -4,8 +4,11 @@
 
 import sys
 import argparse
-import networkx
-import pytest
+import networkx as nx
+
+##### Parcours du graphe de de Bruijn #####
+
+	### Identification des k-mer unique ###
 
 #Lecture du fastq
 def read_fastq(fastq):
@@ -30,58 +33,63 @@ def build_kmer_dict (fastq, kmer_size):
 			else:
 				kmer_dic[kmer] += 1
 	return kmer_dic
-	
+
+
+	### Construction de l’arbre de de Bruijn ###
+
 def build_graph(kmer_dic):
-#	add_edge(node1, node2, weight=2)
+	G = nx.DiGraph()	
+	for kmer, val in kmer_dic.items:
+		G.add_edge(kmer[0:len(kmer)-1], kmer[1:len(kmer)], weight=val)
 	pass
+
+##### Parcours du graphe de de Bruijn #####
 
 def get_starting_nodes():
     pass
 
-
-def std():
-    pass
-
-
 def get_sink_nodes():
     pass
-
-
-def path_average_weight():
-    pass
-
-
-def remove_paths():
-    pass
-
-
-def select_best_path():
-    pass
-
-
-def save_contigs():
-    pass
-
 
 def get_contigs():
     pass
 
+def save_contigs():
+    pass
+
+##### Simplification du graphe de de Bruijn #####
+
+	### Résolution des bulles ###
+	
+def std():
+    pass	
+
+def path_average_weight():
+    pass
+
+def remove_paths():
+    pass
+
+def select_best_path():
+    pass
 
 def solve_bubble():
     pass
 
-
 def simplify_bubbles():
     pass
 
+	### Détection des pointes (tips) ###
 
 def solve_entry_tips():
     pass
 
-
 def solve_out_tips():
     pass
 
+
+
+##### MAIN #####
 def main():
 
 #Récupération des arguments
@@ -91,7 +99,6 @@ def main():
 	parser.add_argument("-o", help= "fichier config")
 	args = parser.parse_args()
 	
-
 
 
 
